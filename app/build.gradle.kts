b333164ec59c8c665b11505c2bad38f582ec67f3
Angelina Zamataeva <angelina.zamataeva2005@yandex.ru>
2023-11-20 19:29:51 +0300

    ADD: объединение элементов, добавление coil, настройка количества элементов

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,8 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
+    implementation("io.coil-kt:coil:2.0.0-rc03")
+    implementation("io.coil-kt:coil-compose:2.0.0-rc03")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
